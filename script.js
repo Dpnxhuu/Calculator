74d45da301174e08e209858644997e8b3620fceb
Deepanshu <dpnxhuu@gmail.com>
2026-01-24 22:30:06 +0530

    fix some error

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
-const input = document.getElementById('input');
-// input.value = '0';
-const operators = ['+','-','*','/','%','=','.'];
-function press(val){
+const input = document.getElementById("input");
 
-    if(input.value === '' && operators.includes(val)){
-        return;
-    }
+const operators = ["+", "-", "*", "/", "%", "=", "."];
+function press(val) {
+  if (input.value === "" && operators.includes(val)) {
+    return;
+  }
 
-    const lastChar = input.value.slice(-1);
+  const lastChar = input.value.slice(-1);
 
-    if(operators.includes(lastChar) && operators.includes(val)) {
-        return;
+  if (operators.includes(lastChar) && operators.includes(val)) {
+    return;
+  } else if (val === "." && input.value.includes(val)) {
+    return;
+  } else if (input.value === "0" && val === "0") {
+    return;
+  } else if (input.value === "0" && !operators.includes(val)) {
+    input.value = val;
+    return;
+  } else {
+    input.value += val;
+    if (input.value.length > 6) {
+      input.style.fontSize = "40px";
+      if (input.value.length > 13) {
+        input.style.fontSize = "24px";
+      }
     }
-    else if(val === '.' && input.value.includes(val)){
-        return;
-    }
-    else if(input.value === '0' && val === '0')
-    {
-        return;
-    }
-    else if(input.value === '0' && !operators.includes(val))
-    {
-        input.value = val;
-        return;
-    }
-    else{
-        input.value+= val;
-    }
+  }
 }
 
 function equal(val) {
-    if(val === 'AC')
-    {
-        input.value = '';
-
+  if (val === "AC") {
+    input.value = "";
+    input.style.fontSize = "70px";
+  } else if (val === "backspace") {
+    input.value = input.value.slice(0, -1);
+    if (input.value.length < 7) {
+      input.style.fontSize = "70px";
     }
-    else if(val === 'backspace')
-    {
-        input.value = input.value.slice(0,-1);
-    }
-    else if(val === 'equal'){
-            input.value = eval(input.value);
-    }
-    else if (val === '-' && input.value[0] !== '-'){
-        input.value = '-'+input.value;
-    }
+  } else if (val === "equal") {
+    input.value = eval(input.value);
+  } else if (val === "-" && input.value[0] !== "-") {
+    input.value = "-" + input.value;
+  }
 }
-
